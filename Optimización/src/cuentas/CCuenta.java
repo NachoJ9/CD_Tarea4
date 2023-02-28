20ea07bd73d4d28b748c65a4a4db19bdaf82f791
Loureiro <Loureiro@DESKTOP-1E259OQ>
2023-02-28 16:49:48 +0100

    Nueva version

--- Optimización/src/cuentas/CCuenta.java
+++ Optimización/src/cuentas/CCuenta.java
@@ -3,71 +3,32 @@ package cuentas;
 public class CCuenta {
 
 
-    public static class CCuentaData {
-		private String nombre;
-		private String cuenta;
-		private double saldo;
-		private double tipoInterés;
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
 
-		public CCuentaData() {
-		}
-
-		public String getNombre() {
-			return nombre;
-		}
-
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
-
-		public String getCuenta() {
-			return cuenta;
-		}
-
-		public void setCuenta(String cuenta) {
-			this.cuenta = cuenta;
-		}
-
-		public double getSaldo() {
-			return saldo;
-		}
-
-		public void setSaldo(double saldo) {
-			this.saldo = saldo;
-		}
-
-		public double getTipoInterés() {
-			return tipoInterés;
-		}
-
-		public void setTipoInterés(double tipoInterés) {
-			this.tipoInterés = tipoInterés;
-		}
-	}
-
-	private CCuentaData data = new CCuentaData();
-
-	public CCuenta()
+    public CCuenta()
     {
     }
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
-        data.setNombre(nom);
-        data.setCuenta(cue);
-        data.setSaldo(sal);
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
     }
 
     public double estado()
     {
-        return data.getSaldo();
+        return saldo;
     }
 
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        data.setSaldo(data.getSaldo() + cantidad);
+        saldo = saldo + cantidad;
     }
 
     public void retirar(double cantidad) throws Exception
@@ -76,6 +37,6 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        data.setSaldo(data.getSaldo() - cantidad);
+        saldo = saldo - cantidad;
     }
 }
