86427f31b0328aa19fc5bd1710b1a4abba58ec7b
NachoJ9 <126559919+NachoJ9@users.noreply.github.com>
2023-03-03 09:20:03 +0100

    Add files via upload

---
+++ documentacion/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"Optimización"}];updateSearchResults();
---
+++ documentacion/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"Optimización","l":"cuentas"}];updateSearchResults();
